f1ae84fc66558233f43bdc9c215af5396d8c0e30
MahrukhBashir03 <163462081+MahrukhBashir03@users.noreply.github.com>
2024-08-22 00:37:19 +0500

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-    return (
-        <div>
-            <h1>Welcome to the Flashcard SaaS</h1>
-            <p>This project uses Next.js, Clerk, Firebase, OpenAI, and Stripe.</p>
-        </div>
-    );
-}
